3431ce347f9d647694488c1e389f6c983e959f93
Joann Lai <lai.joann.cy@gmail.com>
2017-03-15 19:52:25 +0000

    Added username/password feature

--- expressServer.js
+++ expressServer.js
@@ -19,43 +19,78 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs')
 
 const urlsGenerate = require('./views/urlsGenerate') //local path to urlsGenerate function
-//
+
+//database set up
 let urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 }
+let usersDatabase = {
+  "user1RandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  },
+  "1111": {
+     id: "1111",
+     email: "1111email@example.com",
+     password: "1111password"
+   }
+}
 
 //routes
+app.get('/urls', (req, res) => {
+  res.render('urlsIndex', {
+    urls: urlDatabase,
+    userId: req.cookies.userId
+  })
+}) //base index! :)
+
+app.get('/register', (req, res) => {
+  res.render('register')
+}) //registration page
+
+app.post('/register', (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    res.status(400).send('Yikes! Email/password invalid :(')
+  }
+  for (let user in usersDatabase) {
+    if (usersDatabase[user].email === req.body.email) {
+      res.status(400).send('Ooo - account already exists for this email!')
+    }
+  }
+  const id = urlsGenerate()
+  usersDatabase[id]= {}
+  usersDatabase[id]['id'] = id
+  usersDatabase[id]['email'] = req.body.email
+  usersDatabase[id]['password'] = req.body.password
+  res.cookie('userId', usersDatabase[id])
+  res.redirect('/urls')
+}) //collect the userId details
+
 app.get('/urls/new', (req, res) => {
   res.render('urlsNew', {
-    username: req.cookies.username
+    userId: req.cookies.userId
   })
 }) //link to tiny-fy long URL
 
 app.post('/urls', (req, res) => {
   console.log(req.body)  // debug statement to see POST parameters
-  if (req.body.longURL) {
-    let shortURL = urlsGenerate(req.body.longURL)
-    urlDatabase[shortURL] = req.body.longURL
-    res.redirect(`/urls/${shortURL}`)
-  } else if (req.body.username) {
-    console.log(req.body.username)
-    res.cookie('username', req.body.username)
-    res.redirect('/urls')
-  }
+  const shortURL = urlsGenerate()
+  urlDatabase[shortURL] = req.body.longURL
+  res.redirect(`/urls/${shortURL}`)
 }) //adds new short URL
 
 app.post('/logout', (req, res) => {
-  res.clearCookie('username', req.cookies.username)
+  res.clearCookie('userId', req.cookies.userId)
+  console.log(usersDatabase)
   res.redirect('urls')
 })
-
-app.get('/urls', (req, res) => {
-  res.render('urlsIndex', {
-    urls: urlDatabase,
-    username: req.cookies.username
-  })
-}) //base index! :)
 
 app.post('/urls/:id/delete', (req, res) => {
   delete urlDatabase[req.params.id]
@@ -71,7 +106,7 @@ app.get('/urls/:id', (req, res) => {
   res.render('urlsShow', {
     shortURL: req.params.id,
     longURL: urlDatabase[req.params.id],
-    username: req.cookies.username
+    userId: req.cookies.userId
   })
 }) //display one individual URL info
 
--- views/urlsGenerate.js
+++ views/urlsGenerate.js
@@ -1,12 +1,12 @@
 generateRandomString = () => {
-  const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  const alphanumeric = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  const nums = '0123456789'
   const id = []
   let counter = 0
   while (counter < 6) {
-    id.push(charset[Math.floor(Math.random() * charset.length)])
+    id.push(alphanumeric[Math.floor(Math.random() * alphanumeric.length)])
     counter++
   }
   return id.join('');
 }
-
 module.exports = generateRandomString;
